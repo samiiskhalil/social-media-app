4222b82ccc4967ddc79ba732d5b93789b8459eb8
sami~ <samihellstrong@gmail.com>
2022-09-21 22:27:35 +0300

    update

---
+++ server/routes/userRoute.js
@@ -0,0 +1,6 @@
+const {createUser} = require('../controllers/userRoute.js')
+const express = require('express')
+const userRouter=express.Router()
+userRouter.post('/signup',createUser)
+module.exports=userRouter
+//a
