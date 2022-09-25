ba71799c4d6ccec15fa728c8eb5bdf72f36e8c61
sami~ <samihellstrong@gmail.com>
2022-09-25 14:31:16 +0300

    update

--- browser-client/src/components/Signup/Signup.jsx
+++ browser-client/src/components/Signup/Signup.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import { FormControlLabel,InputLabel,Select,MenuItem,TextField,Container } from '@mui/material'
+import { Button,FormControlLabel,InputLabel,Select,MenuItem,TextField,Container } from '@mui/material'
 import {useState,useMemo,useEffect } from 'react'
 const Signup = () => {
-  const [countries,setCountries]=useState([{}])
-
+  const [firstName,setFirstName]=useState('')
+  const [lastName,setLastName]=useState('')
+  const [middleName,setMiddleName]=useState('')
+  const [age,setAge]=useState('')
+  const [date,setDate]=useState('')
+  const [countries,setCountries]=useState([])
+  const [country,setCountry]=useState('')
+  const [city,setCity]=useState('')
+  const [neighborhood,setNeighborhood]=useState('')
   useEffect(()=>{
 fetch('https://restcountries.com/v3.1/all',{
   method:'GET'
@@ -19,25 +26,29 @@ console.log('err')
       [... preState,{name:country.name.common,flag:country.flags.png}] )  )
 })
 },[])
-const getCntryInfo=useMemo(()=>countries.map(country=>
-  <MenuItem  value={country.name}> <img height='20' width='30' src={country.flag} alt='flag'></img> {country.name}</MenuItem>
+const getCntryInfo=useMemo(()=>countries.map((country,id)=>
+  <MenuItem key={id} value={country.name}> <img height='20' width='30' src={country.flag} alt='flag'></img> {country.name}</MenuItem>
   ),[countries])
 return (
 
 <>
 <Container fixed >
   <form >
-<TextField id='firstName' variant='outlined' label="first name" />
-<TextField id='lastName' variant='outlined' label="last name" />
-<TextField id='middleName'  variant='outlined' label="middle name (optional)" />
-<TextField id='age' type='number' min='0' max='130' variant='outlined' label="age" />
-<TextField type='Date' id='birth date' label='birth date' name='birth date'  />
+<TextField id='firstName' required onSubmit={(e)=>{setFirstName(e.target.value)
+  console.log('s')}} variant='outlined' label="first name" />
+<TextField id='lastName' required onSubmit={(e)=>setLastName(e.target.value)} variant='outlined' label="last name" />
+<TextField id='middleName' onSubmit={(e)=>setMiddleName(e.target.value)}  variant='outlined' label="middle name (optional)" />
+<TextField id='age' required type='number' min='0' max='130' variant='outlined' label="age" />
+<TextField type='Date' id='birth date' required name='birth date'  />
 <InputLabel id='country'>country</InputLabel>
-<Select sx={{ minWidth:200 }} labelId='country'   label='country' id='country'>
+<Select sx={{ minWidth:200 }} labelId='country' value={country} label='country' id='country' onChange={e=>setCountry(()=>e.target.value)} >
   {
 getCntryInfo
 }
-  </Select>   
+  </Select  >   
+  <TextField id='city' onSubmit={e=>setCity(e.target.value)} label='city' />
+  <TextField id='neighborhood' onSubmit={e=>setNeighborhood(e.target.value)} label='neighborhood' />
+  <Button type='submit' >submitt</Button>
   </form>
   </Container>
 </>
