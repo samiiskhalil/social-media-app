966c3ccd9e3bd3b209f26fe6f2e946699e6713db
sami~ <samihellstrong@gmail.com>
2022-09-25 13:55:23 +0300

    clinet update

--- browser-client/src/components/Signup/Signup.jsx
+++ browser-client/src/components/Signup/Signup.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-import './signup.css'
-import { TextField,Container } from '@mui/material'
+import { FormControlLabel,InputLabel,Select,MenuItem,TextField,Container } from '@mui/material'
+import {useState,useMemo,useEffect } from 'react'
 const Signup = () => {
-  return (
+  const [countries,setCountries]=useState([{}])
+
+  useEffect(()=>{
+fetch('https://restcountries.com/v3.1/all',{
+  method:'GET'
+}).then(data=>{
+  if(data.ok)
+  return data.json()
+else
+console.log('err')
+})
+.then(response=>{
+  response.forEach(country=>
+    setCountries(preState=>
+      [... preState,{name:country.name.common,flag:country.flags.png}] )  )
+})
+},[])
+const getCntryInfo=useMemo(()=>countries.map(country=>
+  <MenuItem  value={country.name}> <img height='20' width='30' src={country.flag} alt='flag'></img> {country.name}</MenuItem>
+  ),[countries])
+return (
+
 <>
 <Container fixed >
   <form >
@@ -10,7 +31,13 @@ const Signup = () => {
 <TextField id='lastName' variant='outlined' label="last name" />
 <TextField id='middleName'  variant='outlined' label="middle name (optional)" />
 <TextField id='age' type='number' min='0' max='130' variant='outlined' label="age" />
-  <TextField type='Date' />
+<TextField type='Date' id='birth date' label='birth date' name='birth date'  />
+<InputLabel id='country'>country</InputLabel>
+<Select sx={{ minWidth:200 }} labelId='country'   label='country' id='country'>
+  {
+getCntryInfo
+}
+  </Select>   
   </form>
   </Container>
 </>
