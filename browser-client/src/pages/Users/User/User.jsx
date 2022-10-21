792d8d30d63a0e61cdcd14b00d40dd201f2577b2
sami~ <samihellstrong@gmail.com>
2022-10-21 16:59:02 +0300

    update

--- browser-client/src/pages/Users/User/User.jsx
+++ browser-client/src/pages/Users/User/User.jsx
@@ -5,6 +5,10 @@ import './user.css'
 import wallpaper from '../../../resources/sami.jpg'
 import {useEffect,useRef,useState} from 'react'
 const User = () => {
+  const profilePictureUploadInputRef=useRef('')
+  const profilePictureRef=useRef('')
+  const [scaleFlage,setScaleFlage]=useState(false)
+  const [dropDownBackground,setDropDownBackground]=useState(true)
   const [backgroundScale,setBackgroundScale]=useState(1)
   const [backgroundAccept,setBackgroundAccept]=useState(false)
   const [clickPos,setClickPos]=useState([])
@@ -18,30 +22,59 @@ const User = () => {
   const backgroundMenu1=useRef('')
   const [backgroundMenu,setBackgroundMenu]=useState(false)
   const [user, setUser] = useState({})
+  const [profilePictureMenuFlage,setProfilePictureMenuFlage]=useState(false)
+  const[profilePictureEditigFlage,setProfilePictureEditingFlage]=useState(false)
   const handleMouseUpdate=(e)=>{
+    if(editBackground){
     if(backgroundRef.current.clicking){
      setCurrentPos({x:e.pageX- clickPos.x+preOffset.x,y:e.pageY- clickPos.y+preOffset.y})
     backgroundRef.current.style.left=`${currentPos.x}px`
     backgroundRef.current.style.top=`${currentPos.y}px`
+  }}
+  if(profilePictureEditigFlage){
+    if(profilePictureEditigFlage.current.clicking){
+      setCurrentPos({x:e.pageX- clickPos.x+preOffset.x,y:e.pageY- clickPos.y+preOffset.y})
+     profilePictureEditigFlage.current.style.left=`${currentPos.x}px`
+     profilePictureEditigFlage.current.style.top=`${currentPos.y}px`
   }
 }
-
+  }
   function handleMouseUp(e) {
+    if(profilePictureEditigFlage) {
+      profilePictureRef.current.style.cursor='grab'
+      setClickPos({x:e.pageX,y:e.target.pageY})
+      const offsetx=profilePictureRef.current.style.left.split('px')[0]
+      const offsety=profilePictureRef.current.style.top.split('px')[0]
+      setPreOffset({x:currentPos.x,y:currentPos.y})}
+
+    if(editBackground){
     backgroundRef.current.style.cursor='grab'
     backgroundRef.current.clicking=false
    setClickPos({x:e.pageX,y:e.target.pageY})
    const offsetx=backgroundRef.current.style.left.split('px')[0]
    const offsety=backgroundRef.current.style.top.split('px')[0]
-  setPreOffset({x:currentPos.x,y:currentPos.y})
-
-  }
+  setPreOffset({x:currentPos.x,y:currentPos.y})}
+    }
   function handleMouseDown(e){
+    if(profilePictureEditigFlage){
+      profilePictureEditigFlage.current.clicking=true
+   
+      profilePictureEditigFlage.current.style.cursor='grabbing'
+    e.preventDefault()
+    profilePictureEditigFlage.current.clicking=true
+    setClickPos({x:e.pageX,y:e.pageY})
+    }
+    if(editBackground){ 
+
     backgroundRef.current.style.cursor='grabbing'
     e.preventDefault()
     backgroundRef.current.clicking=true
     setClickPos({x:e.pageX,y:e.pageY})
-  }
-    function handleImgUpload(e){
+  }}
+    function handleImgUpload(e){ 
+      backgroundRef.current.style.cursor='grab'
+      setScaleFlage(true)
+      setDropDownBackground(false)
       setEditBackground(true)
       setUploadingBackground(true)
       const [file]=backgroundMenu1.current.files
@@ -50,17 +83,24 @@ const User = () => {
 
   }
 const handlebackgroundConfirm=(e)=>{
+  setScaleFlage(false)
+  setBackgroundMenu(false)
+  setDropDownBackground(true)
   setEditBackground(false)
-  setUploadingBackground(false)
   setBackgroundAccept(true)
-  backgroundRef.currunt.style.cursor='auto'
+  backgroundRef.current.style.cursor='auto'
   //call post method
 }
 const handleCancelBackground=(e)=>{
+  setScaleFlage(true)
+
+  setDropDownBackground(true)
   setUploadingBackground(false)
   setEditBackground(false)
   setBackgroundAccept(false)
   backgroundRef.current.src='#'
+  backgroundRef.current.style.cursor='auto'
+
 }
 const handleScaleDown=(e)=>{
   if(backgroundScale<0.2) return null
@@ -70,6 +110,12 @@ const handleScaleDown=(e)=>{
 const handleScaleUp=(e)=>{
   setBackgroundScale(pre=>pre+0.1)
   backgroundRef.current.style.transform=`scale(${backgroundScale+0.1})`
+}
+const handleProfilePictueUpload=e=>{
+  setProfilePictureEditingFlage(true)
+  const [file]=profilePictureUploadInputRef.current.files
+  profilePictureRef.current.src=URL.createObjectURL(file)
+  console.log(profilePictureRef)
 }
 return (
 <>
@@ -89,19 +135,23 @@ return (
       </div>}
     <img   onMouseOut={(e)=>backgroundRef.current.clicking=false} clicking={false} draggable='false' ref={backgroundRef}
      src='#'  onMouseMove={handleMouseUpdate} onMouseEnter={handleMouseUpdate} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} alt='sbackground-image' className={`background-image ${!editBackground&&'cursor-default'}`}/>
-    <div className={`update-background-container ${uploadingBackground&&'hide-background-opt'} `}>
+     {dropDownBackground&&<div   className='update-background-container  '>
+   <button onClick={()=>setBackgroundMenu(pre=>!pre)} className='  btn-background' ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera" viewBox="0 0 16 16">
+  <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"/>
+  <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+</svg></button>
+  {backgroundMenu&&<div className='background-menu'>
+    <label className='upload-image' htmlFor="background">
 
-    <button onClick={()=>setBackgroundMenu(pre=>!pre)} className='btn-background' >Update</button>
-  {backgroundMenu &&<div className='background-menu'>
-    <label className='edit-image' htmlFor="background">
-
-    <input accept='image/*' onChange={handleImgUpload} ref={backgroundMenu1} placeholder='upload' className='upload-background'  type="file" name="background" id="background" />
+    <input accept='image/*' onChange={handleImgUpload} 
+    ref={backgroundMenu1} placeholder='upload' className='upload-background'  
+    type="file" name="background" id="background" />
     upload
     </label>
    </div>}
 
-  </div>
-   {true&&<div className='scale-container'>
+  </div>}
+   {scaleFlage&&<div className='scale-container'>
       <svg onClick={handleScaleDown} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-out" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
   <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/>
@@ -116,7 +166,24 @@ return (
     </div>}
     </div>
    
+{!editBackground&&<div className="profile-picture-container">
+  <img ref={profilePictureRef} src="#" alt="profile-picture" />
+  <div className="profile-picture-options-container">
+    <button onClick={e=>setProfilePictureMenuFlage(pre=>!pre)}  type='button' className='profile-picture-update-button'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera" viewBox="0 0 16 16">
+  <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"/>
+  <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+</svg>
+    </button >
+      <div className={`profile-picture-update-menu-container ${profilePictureMenuFlage&&'hide'}`}>
+    <label htmlFor="update-profile-picture">
+        <input ref={profilePictureUploadInputRef}   onChange={handleProfilePictueUpload} accept='image/*' placeholder='upload' type='file' name='update-profile-picture' id='update-profile-picture' className=' profile-picture-update-menu-item '/>
+         upload
+    </label>
+      </div>
+  </div>
 
+</div>}
 </div>
 </div>
   </div>
